59cf1c5c4f5bbc6c3e555b5323d10a26d883de6b
Brad Hoffman <bradleydavidhoffman@gmail.com>
2017-12-09 17:02:26 -0500

    added my untested pip-counter code

--- pip-counter.py
+++ pip-counter.py
@@ -1,46 +1,84 @@
-from io import BytesIO
-import PIL.Image
-import PIL.ImageTk
-from Tkinter import *
-import matplotlib
-matplotlib.use('TkAgg')
-from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
-import matplotlib.pyplot as plt
-from picamera import PiCamera
-from time import sleep
+import cv2
+import numpy as np
 
-camera = PiCamera()
-camera.start_preview()
+def count_pips(dice):
+    new_dice = cv2.resize(dice, (150, 150))
+    gray = cv2.cvtColor(new_dice, cv2.COLOR_BGR2GRAY)
+    ret, thresh = cv2.threshold(gray, 150, 255, cv2.THREH_BINARY)
 
-camera.preview.fullscreen = False
-camera.preview.window = (50,50,200,200)
-sleep(2)
-master = Tk()
+    #show image
+    h, w = thresh.shape[:2]
+    mask = np.zeros((h+2, w+2), np.unt8)
+    
+    cv2.floodFill(thresh, mask, (0,0), 255)
+    cv2.floodfill(thresh, mask, (0,149), 255)
+    cv2.floodfill(thresh, mask, (149, 0), 255)
+    cv2.floodfill(thresh, mask, (149, 149), 255)
 
-canvas = Canvas(master,width = 800,height= 600)
-#I don't think this does anything
-canvas.pack()
-back = PIL.Image.new('RGBA', (800,600), color=(255,0,0))
-imageObject = PIL.ImageTk.PhotoImage(image = back)
-canvasObject = canvas.create_image(0,0, anchor = 'nw',image = imageObject)  
+    params = cv2.SipleBlobDetector_Params()
+    params.filterByInertia = True
+    params.minInertiaRatio = 0.5
+    detector = cv2.SimpeBlobDetector_create(params)
 
-def task():
-    stream = BytesIO()
-    camera.capture(stream,format = 'jpeg')
-    stream.seek(0)
-    print(stream)
-    test = PIL.Image.open(stream)
-    test.thumbnail([800,600])
-    image = PIL.ImageTk.PhotoImage(test)
-    #test = PIL.Image.new('RGBA', (800,600), color=(0,255,0))
-    #imageObject = PIL.ImageTk.PhotoImag e(image = test)
-    canvas.itemconfigure(canvasObject,image=image)
-    ##canvas.itemconfig(,image = image)
-    stream.flush()
-    master.after(2000,task) 
+    keypoints = detector.detect(image)
 
-master.after(2000,task)
-mainloop()
-master.quit()
+    return len(keypoints)
+
+#open camera
+cap = cv2.VideoCapture(0)
+
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1920);
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080);
+
+ret, frame = cap.read()
+background = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+while(True):
+    ret, frame = cap.read()
+
+    clone = frame.clone()
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    frame_diff = cv2.absdiff(gray, background)
+    ret, thresh = cv2.threshold(gray, 150, 255, cv2.THRESH_BINARY)
+
+    edges = cv2.Canny(thresh, 2, 4)
+    image, countours, hierarchy = cv2.findContours(thres, cv2.RET_EXTERNAL, cv2.CHAI_APPROX_SIMPLE)
+
+    for contour in contours:
+        area = cv2.contourArea(contour)
+
+        if (area > 2000 and area < 35000):
+            x, y, w, h = cv2.boundingRect(contour)
+            dice = clone[y:y+h, x:x+w]
+
+            num_pips = count_pips(dice)
+
+            if (num_pips > 0):
+                #print some shit
+                #show some rects
 
 
+
+
+#set camera properties
+#take single frame and convert it to grayscale to use when removing background
+#will hold our frame
+
+#while(1):
+    #grab frame from camera
+    #hold unprocessed frame
+    #convert to grayscale
+    #remove background
+    #threshold
+    #applying canny edge filter 
+    #detect dice shapes
+    #iterate over dice contours
+    #get contour area
+    #filter contours based on dice size
+    #get bounding rect
+    #set dice roi
+    #call count function
+    #output pip info
+    #draw rects and values
+    #show
